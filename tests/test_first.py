ec7517fb32547a6587612721973a9a47a1cccef4
heejin131 <journeyjean131@gmail.com>
2025-01-15 09:32:20 +0900

    코드 입력 and 파이테스트 완료

--- tests/test_first.py
+++ tests/test_first.py
@@ -4,10 +4,19 @@ def test_first():
     v = get_os_pretty_name()
     assert v is not None
     assert v == "Ubuntu 24.04.1 LTS"
+    
     # 빈 문자열이 아닌지
-    # 문자열에 LTS가 포함 되었는지
-    # 문자열에 문자도 있고, 숫자도 있는지
-    # . 이 포함 되어 있는지
-    # 기타 등등 ...
-    # 길이가 적어도 얼마 이상인지 ...
+    assert v != ""
 
+    # 문자열에 LTS가 포함 되었는지
+    assert "LTS" in v
+
+    # 문자열에 문자도 있고, 숫자도 있는지
+    assert any(char.isdigit() for char in v)
+    assert any(char.isalpha() for char in v)
+
+    # . 이 포함 되어 있는지
+    assert "." in v
+
+    # 길이가 적어도 5  이상인지 ...
+    assert len(v) >= 5
